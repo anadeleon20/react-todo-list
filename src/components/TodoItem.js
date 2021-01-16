40153ba5ebab5141d005a244606f15fef6ca40ed
genghis.lopez <genghis.lopez@gmail.com>
2021-01-15 21:33:22 -0500

    first commit

---
+++ src/components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+
+
+export default class TodoItem extends Component {
+    render() {
+        return (
+            <h1>
+                Hi from todo item
+            </h1>
+        )
+    }
+}
